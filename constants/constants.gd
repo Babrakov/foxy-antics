67da2224b4bbcc4de3939b4707782902a0cf3a8e
Sergei Babrakov <s.a.babrakov@gmail.com>
2025-03-06 08:36:35 +0100

    Bullet Scene

---
+++ constants/constants.gd
@@ -0,0 +1,11 @@
+class_name Constants
+
+enum ObjectType {
+	EXOLSION,
+	PICKUP,
+	BULLET_PLAYER,
+	BULLET_ENEMY
+}
+
+const PLAYER_GROUP: String = "Player"
+const MOVEABLES_GROUP: String = "Moveables"
