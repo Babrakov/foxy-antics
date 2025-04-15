6ba197268c2e09cf26a51c26ae7373517ae00be3
Sergei Babrakov <s.a.babrakov@gmail.com>
2025-04-15 08:55:02 +0200

    Boss Hit State

--- scenes/boss/boss.gd
+++ scenes/boss/boss.gd
@@ -3,8 +3,23 @@ extends Node2D
 const  TRIGGER_CONDITION: String = "parameters/conditions/on_trigger"
 
 @onready var animation_tree: AnimationTree = $AnimationTree
+@onready var state_machine: AnimationNodeStateMachinePlayback = $AnimationTree["parameters/playback"]
 
+var _invincible: bool = false
 
+func set_invincible(v: bool) -> void:
+	_invincible = v
+	if v == true:
+		state_machine.travel("hit")
+
+func take_damage() -> void:
+	if _invincible == true:
+		return
+		
+	set_invincible(true)
 
 func _on_trigger_area_entered(area: Area2D) -> void:
 	animation_tree[TRIGGER_CONDITION] = true
+
+func _on_hit_box_area_entered(area: Area2D) -> void:
+	take_damage()
