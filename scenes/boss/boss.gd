027da23b9faa73ec7b17b9feda93d5d47a8f7fe4
Sergei Babrakov <s.a.babrakov@gmail.com>
2025-04-17 08:57:48 +0200

    Boss Hit Tween

--- scenes/boss/boss.gd
+++ scenes/boss/boss.gd
@@ -2,10 +2,15 @@ extends Node2D
 
 const  TRIGGER_CONDITION: String = "parameters/conditions/on_trigger"
 
+@onready var visual: Node2D = $Visual
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var state_machine: AnimationNodeStateMachinePlayback = $AnimationTree["parameters/playback"]
 
 var _invincible: bool = false
+
+func tween_hit() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(visual, "position", Vector2.ZERO, 1.6)
 
 func set_invincible(v: bool) -> void:
 	_invincible = v
@@ -17,6 +22,7 @@ func take_damage() -> void:
 		return
 		
 	set_invincible(true)
+	tween_hit()
 
 func _on_trigger_area_entered(area: Area2D) -> void:
 	animation_tree[TRIGGER_CONDITION] = true
